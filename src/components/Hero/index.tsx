922a5faaffb697c51ffc494b4f02ea83666439ad
Maurizio Fiaschini <maurizio.fiaschini@akqa.com>
2025-04-08 18:15:42 +0200

    [#32] feat: add textstr field to HeroModel to text datocms field

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,25 +1,27 @@
 "use client";
+import { StructuredText, type StructuredTextDocument } from "react-datocms";
 
 import { HeroRecord } from "@/graphql/generated";
 import {
   Hero as HeroComponent,
   HeroBody,
-  HeroCategory,
+  // HeroCategory,
   HeroTitle,
   HeroButton,
 } from "design-react-kit";
 
 export function Hero({ props }: { props: HeroRecord }) {
-  const { title, text } = props;
+  const { title, text, textstr } = props;
   return (
     <HeroComponent>
       <HeroBody>
-        <HeroCategory>Category</HeroCategory>
+        {/* <HeroCategory>Category</HeroCategory> */}
         {title && <HeroTitle>{title}</HeroTitle>}
         {text && <p className="d-none d-lg-block">{text}</p>}
-        <HeroButton color="primary" outline>
-          Label button
-        </HeroButton>
+        <div className="d-none d-lg-block">
+          <StructuredText data={textstr?.value as StructuredTextDocument} />
+        </div>
+        <HeroButton color="primary">Label button</HeroButton>
       </HeroBody>
     </HeroComponent>
   );
