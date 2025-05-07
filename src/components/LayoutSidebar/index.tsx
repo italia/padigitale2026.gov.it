de7530bf7703758304c9025476cad189f7df4066
Maurizio Fiaschini <maurizio.fiaschini@akqa.com>
2025-05-07 18:29:44 +0200

    fix: hydratation problem for sticky behavior

--- src/components/LayoutSidebar/index.tsx
+++ src/components/LayoutSidebar/index.tsx
@@ -3,17 +3,27 @@
 import { LayoutSidebarRecord } from "@/graphql/generated";
 import { RichText } from "@/src/components/RichText";
 import { NavScroll } from "@/src/components/NavScroll";
+import { useEffect, useState } from "react";
 
 export function LayoutSidebar({ props }: { props: LayoutSidebarRecord }) {
   const { sidebar, content } = props;
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   return (
     <div className="container-xxl py-lg-5">
       <div className="row">
         <div className="col-12 col-lg-4">
-          <div data-bs-toggle="sticky" data-bs-stackable="true">
-            {sidebar && <NavScroll props={sidebar} />}
-          </div>
+          {isClient ? (
+            <div data-bs-toggle="sticky" data-bs-stackable="true">
+              {sidebar && <NavScroll props={sidebar} />}
+            </div>
+          ) : (
+            <div>{sidebar && <NavScroll props={sidebar} />}</div>
+          )}
         </div>
         <div className="col-12 col-lg-8 it-page-sections-container">
           {content.map((item, index) => (
