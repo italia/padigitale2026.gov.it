c8dca093b0d2444cc70a5020caf05e961b5c6c99
Maurizio Fiaschini <maurizio.fiaschini@akqa.com>
2025-05-08 11:30:20 +0200

    fix: add type and props for CardGeneric CardsGrid CardResource

--- src/components/CardResource/index.tsx
+++ src/components/CardResource/index.tsx
@@ -1,36 +1,47 @@
 "use client";
 
-import {ResourceRecord} from "@/graphql/generated";
+import { ResourceRecord } from "@/graphql/generated";
 import Link from "next/link";
 import styles from "./index.module.scss";
 import classNames from "classnames/bind";
+import { ElementType } from "react";
+
 const cn = classNames.bind(styles);
 
-export function CardResource({ props, TitleTag = 'div'}: { props: ResourceRecord, TitleTag?: string }) {
+export function CardResource({
+  props,
+  TitleTag = "div",
+}: {
+  props: ResourceRecord;
+  TitleTag?: ElementType;
+}) {
   const { title, summary, badge, data, slug } = props;
   return (
     <article className="it-card pb-0 bg-white flex-grow-1">
       {title && (
         <TitleTag className="it-card-title px-0 fs-3 fw-semibold lh-sm">
-          <Link href={`/${slug}`} className={cn('decoration-1')}>{title}</Link>
+          <Link href={`/${slug}`} className={cn("decoration-1")}>
+            {title}
+          </Link>
         </TitleTag>
       )}
       <div className="it-card-body px-0 pb-2 d-flex flex-column pt-3 pb-3 border-bottom border-neutral-1-bg-a3">
-        {summary && (
-          <p className="it-card-text fs-6 flex-grow-1">{summary}</p>
-        )}
+        {summary && <p className="it-card-text fs-6 flex-grow-1">{summary}</p>}
         <footer className="it-card-related pb-2">
           {badge && (
             <div className={"it-card-taxonomy"}>
               <span className="visually-hidden">Tag correlato: </span>
-              <span className={cn("badge text-primary fs-6 text-capitalize px-3", "bg-badge")}>
+              <span
+                className={cn(
+                  "badge text-primary fs-6 text-capitalize px-3",
+                  "bg-badge"
+                )}
+              >
                 {badge}
               </span>
             </div>
           )}
-          {data && (
-            <time className={"it-card-date"}>{data}</time>
-          )}
+          {data && <time className={"it-card-date"}>{data}</time>}
         </footer>
       </div>
     </article>
--- src/components/CardsGrid/index.tsx
+++ src/components/CardsGrid/index.tsx
@@ -1,42 +1,62 @@
 "use client";
 
-import {CardsGridRecord} from "@/graphql/generated";
-
+import { CardsGridRecord } from "@/graphql/generated";
 import styles from "./index.module.scss";
 import classNames from "classnames/bind";
-import {CardResource} from "@/src/components/CardResource";
+import { CardResource } from "@/src/components/CardResource";
 import Link from "next/link";
-import {Icon} from "design-react-kit";
-import {cardAspectEnum, CardGeneric} from "@/src/components/CardGeneric";
+import { Icon } from "design-react-kit";
+import { cardAspectEnum, CardGeneric } from "@/src/components/CardGeneric";
+import { ElementType } from "react";
 
 const cn = classNames.bind(styles);
 
 export function CardsGrid({ props }: { props: CardsGridRecord }) {
-  const { title, description, alignment, risorse, news, columns, button, background, cardTitleTag, customCards } = props;
+  const {
+    title,
+    description,
+    alignment,
+    risorse,
+    news,
+    columns,
+    button,
+    background,
+    cardTitleTag,
+    customCards,
+  } = props;
+  const titleTag = cardTitleTag || "h3";
   return (
     <div className={`wrapper py-5 ${background}`}>
       <div className={cn("row w-100 h-100 mx-auto container-xxl")}>
         <div className="col-12 pb-3">
           {title && (
-            <h2 className={cn(
-              "text-dark mb-0 fs-2 lh-sm",
-              alignment === "center" ? "text-center" : "text-start"
-            )}>{title}</h2>
+            <h2
+              className={cn(
+                "text-dark mb-0 fs-2 lh-sm",
+                alignment === "center" ? "text-center" : "text-start"
+              )}
+            >
+              {title}
+            </h2>
           )}
           {description && (
-            <p className={cn(
-              "font-sans-serif text-dark mt-3 mb-0",
-              alignment === "center" ? "text-center" : "text-start"
-            )}>{description}</p>
+            <p
+              className={cn(
+                "font-sans-serif text-dark mt-3 mb-0",
+                alignment === "center" ? "text-center" : "text-start"
+              )}
+            >
+              {description}
+            </p>
           )}
         </div>
       </div>
       {risorse && (
-        <div className={'row w-100 h-100 mx-auto container-xxl'}>
+        <div className={"row w-100 h-100 mx-auto container-xxl"}>
           {risorse.map((resource, idx) => {
             let colClasses = "";
             const intColumns = (columns && parseInt(columns)) ?? 1;
-            if (intColumns  === 1) {
+            if (intColumns === 1) {
               colClasses = "col-12";
             } else if (intColumns === 2) {
               colClasses = "col-12 col-md-6";
@@ -46,19 +66,25 @@ export function CardsGrid({ props }: { props: CardsGridRecord }) {
               colClasses = "col-12 col-lg-3";
             }
             return (
-              <div key={idx} className={`${colClasses} pt-4 d-flex flex-column justify-content-stretch`}>
-                <CardResource TitleTag={cardTitleTag || 'h3'} props={resource} />
+              <div
+                key={idx}
+                className={`${colClasses} pt-4 d-flex flex-column justify-content-stretch`}
+              >
+                <CardResource
+                  TitleTag={titleTag as ElementType}
+                  props={resource}
+                />
               </div>
             );
           })}
         </div>
       )}
       {news && (
-        <div className={'row w-100 h-100 mx-auto container-xxl'}>
+        <div className={"row w-100 h-100 mx-auto container-xxl"}>
           {news.map((record, idx) => {
             let colClasses = "";
             const intColumns = (columns && parseInt(columns)) ?? 1;
-            if (intColumns  === 1) {
+            if (intColumns === 1) {
               colClasses = "col-12";
             } else if (intColumns === 2) {
               colClasses = "col-12 col-md-6";
@@ -68,19 +94,26 @@ export function CardsGrid({ props }: { props: CardsGridRecord }) {
               colClasses = "col-12 col-lg-3";
             }
             return (
-              <div key={idx} className={`${colClasses} pt-4 d-flex flex-column justify-content-stretch`}>
-                <CardGeneric TitleTag={cardTitleTag || 'h3'} cardAspect={cardAspectEnum.clean} props={record} />
+              <div
+                key={idx}
+                className={`${colClasses} pt-4 d-flex flex-column justify-content-stretch`}
+              >
+                <CardGeneric
+                  TitleTag={titleTag as ElementType}
+                  cardAspect={cardAspectEnum.clean}
+                  props={record}
+                />
               </div>
             );
           })}
         </div>
       )}
       {customCards && customCards.cards && (
-        <div className={'row w-100 h-100 mx-auto container-xxl'}>
+        <div className={"row w-100 h-100 mx-auto container-xxl"}>
           {customCards.cards.map((card, idx) => {
             let colClasses = "";
             const intColumns = (columns && parseInt(columns)) ?? 1;
-            if (intColumns  === 1) {
+            if (intColumns === 1) {
               colClasses = "col-12";
             } else if (intColumns === 2) {
               colClasses = "col-12 col-md-6";
@@ -91,19 +124,37 @@ export function CardsGrid({ props }: { props: CardsGridRecord }) {
             }
 
             return (
-              <div key={idx} className={`${colClasses} pt-4 d-flex flex-column justify-content-stretch`}>
-                <CardGeneric TitleTag={cardTitleTag || 'h3'} cardAspect={cardAspectEnum[(customCards.cardLayout ?? 'bordered')]} props={card} />
+              <div
+                key={idx}
+                className={`${colClasses} pt-4 d-flex flex-column justify-content-stretch`}
+              >
+                <CardGeneric
+                  TitleTag={titleTag as ElementType}
+                  cardAspect={
+                    cardAspectEnum[
+                      (customCards.cardLayout ??
+                        "bordered") as keyof typeof cardAspectEnum
+                    ]
+                  }
+                  props={card}
+                />
               </div>
             );
           })}
         </div>
       )}
-      {(button && (
-        <div className={'row w-100 h-100 mx-auto container-xxl'}>
-          <div className={cn("col-12 pt-5", alignment === "center" ? "text-center" : "text-start")}>
+      {button && (
+        <div className={"row w-100 h-100 mx-auto container-xxl"}>
+          <div
+            className={cn(
+              "col-12 pt-5",
+              alignment === "center" ? "text-center" : "text-start"
+            )}
+          >
             <Link
               href={button.href || `/${button.cmsPage?.slug || ""}`}
-              className={'btn btn-outline-primary btn-lg'}>
+              className={"btn btn-outline-primary btn-lg"}
+            >
               <span>{button.text}</span>
               {button.icon && (
                 <Icon
@@ -118,8 +169,7 @@ export function CardsGrid({ props }: { props: CardsGridRecord }) {
             </Link>
           </div>
         </div>
-      ))}
-
+      )}
     </div>
   );
 }
