aaa37174e40afc9a7fc263a0acf23eabb1f4bea6
Matteo Rosati <rosati.matteo@gmail.com>
2025-05-21 11:46:08 +0200

    add revalidate = 0 in hp controller

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { page } from "@/lib/datocms";
 import { ModularContent } from "@/src/components/ModularContent";
 import { PageQuery } from "@/graphql/generated";
 
+export const revalidate = 0;
+
 export default async function Page() {
   const { isEnabled } = await draftMode(); // get draft content or not
   const content = (await page("homepage", isEnabled)) as PageQuery;
