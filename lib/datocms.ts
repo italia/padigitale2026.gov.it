4da5ffc04783cd7e5bd4cb0feb8c76266c2f7d44
Matteo Rosati <rosati.matteo@gmail.com>
2025-05-21 15:54:41 +0200

    add referer

--- lib/datocms.ts
+++ lib/datocms.ts
@@ -1,8 +1,23 @@
-import { executeQuery, executeQueryWithAutoPagination } from '@datocms/cda-client';
-import { AllPagesDocument, AllFaqsDocument, PageDocument, SitemapPagesDocument, PageQueryVariables, FooterDocument, HeaderDocument, AllNewsDocument, AllResourcesDocument } from '@/graphql/generated';
+import {
+  executeQuery,
+  executeQueryWithAutoPagination,
+} from "@datocms/cda-client";
+import {
+  AllPagesDocument,
+  AllFaqsDocument,
+  PageDocument,
+  SitemapPagesDocument,
+  PageQueryVariables,
+  FooterDocument,
+  HeaderDocument,
+  AllNewsDocument,
+  AllResourcesDocument,
+} from "@/graphql/generated";
 
 if (!process.env.DATOCMS_API_TOKEN) {
-  throw new Error("DatoCMS API Token is not defined. Please check your .env file and next.config.js");
+  throw new Error(
+    "DatoCMS API Token is not defined. Please check your .env file and next.config.js"
+  );
 }
 
 const options = {
@@ -17,42 +32,61 @@ export async function getAllPages(includeDrafts: boolean = false) {
 }
 
 export async function page(slug: string, includeDrafts: boolean = false) {
-  options.includeDrafts = includeDrafts;
   return executeQuery(PageDocument, {
     ...options,
+    includeDrafts: includeDrafts,
+    referer: `function:page|slug:${slug}|includeDrafts:${includeDrafts}`,
     variables: {
       slug: slug,
-      index: "2"
-    } as PageQueryVariables
-  })
+      index: "2",
+    } as PageQueryVariables,
+  });
 }
 
 export async function getAllFaqs(includeDrafts: boolean = false) {
-  options.includeDrafts = includeDrafts;
-  return executeQueryWithAutoPagination(AllFaqsDocument, options);
+  return executeQueryWithAutoPagination(AllFaqsDocument, {
+    ...options,
+    includeDrafts: includeDrafts,
+    referer: `function:getAllFaqs|includeDrafts:${includeDrafts}`,
+  });
 }
 
 export async function getAllNews(includeDrafts: boolean = false) {
-  options.includeDrafts = includeDrafts;
-  return executeQueryWithAutoPagination(AllNewsDocument, options);
+  return executeQueryWithAutoPagination(AllNewsDocument, {
+    ...options,
+    includeDrafts: includeDrafts,
+    referer: `function:getAllNews|includeDrafts:${includeDrafts}`,
+  });
 }
 
 export async function getAllResources(includeDrafts: boolean = false) {
-  options.includeDrafts = includeDrafts;
-  return executeQueryWithAutoPagination(AllResourcesDocument, options);
+  return executeQueryWithAutoPagination(AllResourcesDocument, {
+    ...options,
+    includeDrafts: includeDrafts,
+    referer: `function:getAllResources|includeDrafts:${includeDrafts}`,
+  });
 }
 
 export async function getFooter(includeDrafts: boolean = false) {
-  options.includeDrafts = includeDrafts;
-  return executeQuery(FooterDocument, options);
+  return executeQuery(FooterDocument, {
+    ...options,
+    includeDrafts: includeDrafts,
+    referer: `function:getFooter|includeDrafts:${includeDrafts}`,
+  });
 }
 
 export async function getHeader(includeDrafts: boolean = false) {
-  options.includeDrafts = includeDrafts;
-  return executeQuery(HeaderDocument, options);
+  return executeQuery(HeaderDocument, {
+    ...options,
+    includeDrafts: includeDrafts,
+    referer: `function:getHeader|includeDrafts:${includeDrafts}`,
+  });
 }
 
 export async function getSitemapPages(includeDrafts: boolean = false) {
-  options.includeDrafts = includeDrafts;
-  return executeQueryWithAutoPagination(SitemapPagesDocument, options);
+  return executeQueryWithAutoPagination(SitemapPagesDocument, {
+    ...options,
+    includeDrafts: includeDrafts,
+    referer: `function:getSitemapPages|includeDrafts:${includeDrafts}`,
+  });
 }
