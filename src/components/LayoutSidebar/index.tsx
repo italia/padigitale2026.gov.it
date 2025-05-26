16bd359abdc9cf62048982d7f5ec3877615b8bd3
Maurizio Fiaschini <maurizio.fiaschini@akqa.com>
2025-05-26 17:21:57 +0200

    [#48] feature: hydratation management and code optimization

--- src/components/LayoutSidebar/index.tsx
+++ src/components/LayoutSidebar/index.tsx
@@ -1,32 +1,20 @@
-"use client";
-
 import { LayoutSidebarRecord } from "@/graphql/generated";
 import { RichText } from "@/src/components/RichText";
 import { NavScroll } from "@/src/components/NavScroll";
 import { StepperAccordion } from "@/src/components/StepperAccordion";
-import { useEffect, useState } from "react";
 import { Col } from "design-react-kit";
 import { Fragment } from "react";
 
 export function LayoutSidebar({ props }: { props: LayoutSidebarRecord }) {
   const { sidebar, content } = props;
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
 
   return (
     <div className="container-xxl py-lg-5">
       <div className="row">
         <div className="col-12 col-lg-4">
-          {isClient ? (
-            <div data-bs-toggle="sticky" data-bs-stackable="true">
-              {sidebar && <NavScroll props={sidebar} />}
-            </div>
-          ) : (
-            <div>{sidebar && <NavScroll props={sidebar} />}</div>
-          )}
+          <div data-bs-toggle="sticky" data-bs-stackable="true">
+            {sidebar && <NavScroll props={sidebar} />}
+          </div>
         </div>
         <div className="col-12 col-lg-8 it-page-sections-container">
           {content.map((item, index) => (
