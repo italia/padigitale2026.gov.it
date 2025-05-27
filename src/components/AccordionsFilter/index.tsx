1b52e9e107e88469b4200a7aece2320064aef4c2
Maurizio Fiaschini <maurizio.fiaschini@akqa.com>
2025-05-27 16:56:58 +0200

    [#48] feat: work in progress (fragment modified)

--- src/components/AccordionsFilter/index.tsx
+++ src/components/AccordionsFilter/index.tsx
@@ -1,15 +1,30 @@
 "use client";
 
-import { AccordionsFilterRecord } from "@/graphql/generated";
+import {
+  AccordionsFilterRecord,
+  CardsGridResourceRecord,
+} from "@/graphql/generated";
 import { usePages } from "@/src/contexts/PagesContext";
 import { useState, Fragment } from "react";
-import { Select } from "design-react-kit";
+import {
+  Select,
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionBody,
+} from "design-react-kit";
+
+import { CardsGrid } from "@/src/components/CardsGrid";
+
+import styles from "./index.module.scss";
+import classNames from "classnames/bind";
+const cn = classNames.bind(styles);
 
 export function AccordionsFilter({ props }: { props: AccordionsFilterRecord }) {
   const { items } = props;
   const { misuras, enteBeneficiarios } = usePages();
   const [value, setValue] = useState<string>("");
-  // const [hasVisibleContent, setHasVisibleContent] = useState(true);
+  const [collapseElementOpen, setCollapseElement] = useState("");
   const handleChange = (selectedOption: string) => {
     setValue(selectedOption);
     console.log(value);
@@ -27,80 +42,10 @@ export function AccordionsFilter({ props }: { props: AccordionsFilterRecord }) {
       .replace(/(^-|-$)/g, "");
   };
 
-  // useEffect(() => {
-  //   // Reset visibility state at the start of each filter change
-  //   setHasVisibleContent(false);
-
-  //   // Filter management
-  //   const elements = document.querySelectorAll("[data-beneficiari]");
-  //   let hasVisibleElements = false;
-
-  //   elements.forEach((element) => {
-  //     if (!value) {
-  //       // If no value is selected, show all elements
-  //       element.classList.remove("d-none");
-  //       hasVisibleElements = true;
-  //     } else {
-  //       const beneficiari =
-  //         element.getAttribute("data-beneficiari")?.split(" ") || [];
-  //       if (beneficiari.includes(value)) {
-  //         element.classList.remove("d-none");
-  //         hasVisibleElements = true;
-  //       } else {
-  //         element.classList.add("d-none");
-  //       }
-  //     }
-  //   });
-
-  //   // Container visibility management
-  //   const containers = document.querySelectorAll(".it-page-section");
-
-  //   containers.forEach((container) => {
-  //     const visibleElements = container.querySelectorAll(
-  //       "[data-beneficiari]:not(.d-none)"
-  //     );
-  //     const containerId = container.id;
-
-  //     if (visibleElements.length === 0 && value !== "") {
-  //       container.classList.add("d-none");
-  //       // Disable sidebar link pointing to this section
-  //       if (containerId) {
-  //         const sidebarLink = document.querySelector(
-  //           `a[href="#${containerId}"]`
-  //         );
-  //         if (sidebarLink) {
-  //           sidebarLink.classList.remove("active");
-  //           sidebarLink.classList.add("disabled");
-  //           sidebarLink.setAttribute("aria-disabled", "true");
-  //           (sidebarLink as HTMLElement).style.pointerEvents = "none";
-  //           (sidebarLink as HTMLElement).style.opacity = "0.5";
-  //         }
-  //       }
-  //     } else {
-  //       container.classList.remove("d-none");
-  //       // Re-enable sidebar link
-  //       if (containerId) {
-  //         const sidebarLink = document.querySelector(
-  //           `a[href="#${containerId}"]`
-  //         );
-  //         if (sidebarLink) {
-  //           sidebarLink.classList.remove("disabled");
-  //           sidebarLink.removeAttribute("aria-disabled");
-  //           (sidebarLink as HTMLElement).style.pointerEvents = "";
-  //           (sidebarLink as HTMLElement).style.opacity = "";
-  //         }
-  //       }
-  //     }
-  //   });
-
-  //   // Update visibility state after all DOM manipulations
-  //   setHasVisibleContent(hasVisibleElements);
-  // }, [value]);
-
   return (
-    <div className="container-xxl py-lg-5">
+    <div className={cn("container-xxl py-lg-5", styles.accordionsFilter)}>
       <div className="row" style={{ marginTop: "64px", marginBottom: "48px" }}>
-        <div className="col-12 d-flex gap-4">
+        <div className="col-12 col-lg-4">
           <Select
             id="example-reactstrap"
             label="Misura"
@@ -118,7 +63,8 @@ export function AccordionsFilter({ props }: { props: AccordionsFilterRecord }) {
               ))}
             </>
           </Select>
-
+        </div>
+        <div className="col-12 col-lg-4">
           <Select
             id="example-reactstrap"
             label="Beneficiario"
@@ -140,28 +86,34 @@ export function AccordionsFilter({ props }: { props: AccordionsFilterRecord }) {
       </div>
       <div className="row">
         <div className="col-12 it-page-sections-container">
-          {/* {!hasVisibleContent && value !== "" && (
-            <div className="alert alert-info" role="alert">
-              <p className="mb-0">
-                Non ci sono contenuti disponibili per il beneficiario
-                selezionato.
-              </p>
-            </div>
-          )} */}
           {items.map((item, index) => (
-            <Fragment key={index}>
-              <div
-                className="row it-page-section pb-4"
-                // id={item.anchorId || undefined}
-              >
-                <h2>{item.title}</h2>
-                <div className="row">
-                  {/* {item.cards.map((card, index) => (
-                    <div key={index}>{card.title}</div>
-                  ))} */}
-                </div>
-              </div>
-            </Fragment>
+            <Accordion iconLeft key={index}>
+              <AccordionItem>
+                <AccordionHeader
+                  className="h2"
+                  active={collapseElementOpen === index.toString()}
+                  onToggle={() =>
+                    setCollapseElement(
+                      collapseElementOpen !== index.toString()
+                        ? index.toString()
+                        : ""
+                    )
+                  }
+                >
+                  {item.title}
+                </AccordionHeader>
+                <AccordionBody
+                  active={collapseElementOpen === index.toString()}
+                >
+                  {item.cards && (
+                    <CardsGrid
+                      props={item.cards as CardsGridResourceRecord}
+                      hasSidebar={false}
+                    />
+                  )}
+                </AccordionBody>
+              </AccordionItem>
+            </Accordion>
           ))}
         </div>
       </div>
