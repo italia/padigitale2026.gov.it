d3bc0a582fb514606a18449159c3b9f1d2fdd3f8
Maurizio Fiaschini <maurizio.fiaschini@akqa.com>
2025-06-10 10:37:26 +0200

    [#80] feat: minor ui improvements

--- src/components/TableList/index.tsx
+++ src/components/TableList/index.tsx
@@ -41,7 +41,7 @@ export function TableList({
 
   return (
     <div
-      className="container-xxl"
+      className={cn("container-xxl", { "my-5": !noPadding })}
       role="region"
       aria-labelledby={title ? `${id}-title` : undefined}
     >
