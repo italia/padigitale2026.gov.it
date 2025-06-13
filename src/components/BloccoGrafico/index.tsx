8642e4f7381f68420a43cd833acb05f3de5a341e
Maurizio Fiaschini <maurizio.fiaschini@akqa.com>
2025-06-13 09:36:17 +0200

    [#70] feat: work in progress blocco grafico

--- src/components/BloccoGrafico/index.tsx
+++ src/components/BloccoGrafico/index.tsx
@@ -1,18 +1,32 @@
 import { BloccoGraficoRecord } from "@/graphql/generated";
 
+import ReactEcharts from "echarts-for-react";
+
 import styles from "./index.module.scss";
 import classNames from "classnames/bind";
 const cn = classNames.bind(styles);
 
 export function BloccoGrafico({ props }: { props: BloccoGraficoRecord }) {
-  const { subtitle, title } = props;
+  const { subtitle, title, chart } = props;
+
   return (
     <div className={cn("wrapper", "p-0")}>
       <div className={cn("container-xxl")}>
-        {/* Body */}
         <div className="row h-100 py-4">
           {title && <h2 className={"col-12 mb-3 fs-2 lh-sm"}>{title}</h2>}
           {subtitle && <p className={"col-12 font-sans-serif"}>{subtitle}</p>}
+          <h3>{chart?.title}</h3>
+          {chart && (
+            <ReactEcharts
+              key={chart.id}
+              option={chart.chartData}
+              style={{
+                height: "400px",
+                width: "100%",
+                border: "1px solid red",
+              }}
+            />
+          )}
         </div>
       </div>
     </div>
