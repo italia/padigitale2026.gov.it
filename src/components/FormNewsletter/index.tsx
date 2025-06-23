233e1d7a7cc316e38fe7ce3cee8af2ea728e346e
Maurizio Fiaschini <maurizio.fiaschini@akqa.com>
2025-06-23 18:00:54 +0200

    [#92] feat: add some logic to show/hide fields

--- src/components/FormNewsletter/index.tsx
+++ src/components/FormNewsletter/index.tsx
@@ -4,12 +4,48 @@ import Link from "next/link";
 import { Button, Input, Select } from "design-react-kit";
 import { Row } from "design-react-kit";
 import { Col } from "design-react-kit";
+import { useState } from "react";
 
 import classNames from "classnames/bind";
 import styles from "./index.module.scss";
 const cn = classNames.bind(styles);
 
 export function FormNewsletter() {
+  const [formState, setFormState] = useState({
+    email: "",
+    rappresento: "",
+    tipoEnte: "",
+    nomeStruttura: "",
+    inQuanto: "",
+    // Campi condizionali
+    showTipoEnte: false,
+    showNomeStruttura: false,
+  });
+
+  const shouldShowTipoEnte =
+    formState.rappresento === "pubblica-amministrazione";
+  const shouldShowNomeStruttura = formState.rappresento === "altro";
+
+  // Logica per abilitare il bottone
+  const isFormValid = () => {
+    const requiredFields = [
+      formState.email,
+      formState.rappresento,
+      formState.nomeStruttura,
+    ];
+
+    // Aggiungi campi condizionali se visibili
+    if (shouldShowNomeStruttura) {
+      requiredFields.push(formState.tipoEnte);
+    }
+    if (shouldShowTipoEnte) {
+      requiredFields.push(formState.inQuanto);
+    }
+
+    console.log("requiredFields", requiredFields);
+    return requiredFields.every((field) => field && field.length > 0);
+  };
+
   return (
     <div className={cn("wrapper", "container-xxl py-5 my-5 mx-auto")}>
       <div className="row">
@@ -18,13 +54,28 @@ export function FormNewsletter() {
 
           <Row className="mt-5">
             <Col md="6">
-              <Input id="inputEmail" label="Email*" type="email" />
+              <Input
+                id="inputEmail"
+                label="Email*"
+                type="email"
+                onChange={(e) => {
+                  setFormState({
+                    ...formState,
+                    email: e.target.value,
+                  });
+                }}
+              />
             </Col>
             <Col md="6">
               <Select
                 id="selectRepresent"
                 label="Rappresento*"
-                onChange={() => {}}
+                onChange={(value) => {
+                  setFormState({
+                    ...formState,
+                    rappresento: value,
+                  });
+                }}
               >
                 <option label="Scegli una voce dall'elenco"></option>
                 <option label="Pubblica amministrazione">
@@ -35,27 +86,63 @@ export function FormNewsletter() {
               </Select>
             </Col>
           </Row>
+          {shouldShowNomeStruttura && (
+            <Row className="mt-5">
+              <Input
+                id="inputEnte"
+                label="Tipo di ente/struttura*"
+                type="text"
+                onChange={(e) => {
+                  setFormState({
+                    ...formState,
+                    tipoEnte: e.target.value,
+                  });
+                }}
+              />
+            </Row>
+          )}
           <Row className="mt-5">
-            <Input id="inputEnte" label="Tipo di ente/struttura*" type="text" />
+            <Input
+              id="inputName"
+              label="Nome struttura*"
+              type="text"
+              onChange={(e) => {
+                setFormState({
+                  ...formState,
+                  nomeStruttura: e.target.value,
+                });
+              }}
+            />
           </Row>
-          <Row className="mt-5">
-            <Input id="inputName" label="Nome struttura*" type="text" />
-          </Row>
-          <Row className="mt-5">
-            <Select id="selectEnte" label="In quanto*" onChange={() => {}}>
-              <option label="Scegli una voce dall'elenco"></option>
-              <option label="Dirigente dell'amministrazione">
-                dirigente-dell-amministrazione
-              </option>
-              <option label="Dirigente sistemi IT dell'amministrazione">
-                dirigente-sistemi-it-dell-amministrazione
-              </option>
-              <option label="Dipendente dell'amministrazione">
-                dipendente-dell-amministrazione
-              </option>
-              <option label="Altro">altro</option>
-            </Select>
-          </Row>
+
+          {shouldShowTipoEnte && (
+            <Row className="mt-5">
+              <Select
+                id="selectEnte"
+                label="In quanto*"
+                onChange={(value) => {
+                  console.log("value", value);
+                  setFormState({
+                    ...formState,
+                    inQuanto: value,
+                    showTipoEnte: true,
+                  });
+                }}
+              >
+                <option label="Scegli una voce dall'elenco"></option>
+                <option label="Dirigente dell'amministrazione">
+                  dirigente-dell-amministrazione
+                </option>
+                <option label="Dirigente sistemi IT dell'amministrazione">
+                  dirigente-sistemi-it-dell-amministrazione
+                </option>
+                <option label="Dipendente dell'amministrazione">
+                  dipendente-dell-amministrazione
+                </option>
+                <option label="Altro">altro</option>
+              </Select>
+            </Row>
+          )}
 
           <p className="text-muted mt-5">
             Cliccando su INVIA dichiaro di aver letto e compreso{" "}
@@ -69,7 +156,14 @@ export function FormNewsletter() {
                 </Button>
               </Col> */}
             <Col sm="auto">
-              <Button color="primary" type="submit" disabled={true}>
+              <Button
+                color="primary"
+                type="submit"
+                disabled={!isFormValid()}
+                onClick={() => {
+                  console.log("formState", formState);
+                }}
+              >
                 Invia
               </Button>
             </Col>
