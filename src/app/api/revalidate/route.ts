d26db009e890d465226a4928b9219b2b1c152ce6
Matteo Rosati <rosati.matteo@gmail.com>
2025-07-14 10:05:10 +0200

    add layout to revalidate

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,10 +2,10 @@ import { NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
 export function POST() {
-    revalidatePath("/");
+  revalidatePath("/", "layout");
 
-    return NextResponse.json({
-        revalidate: true,
-        timestamp: Date.now()
-    })
+  return NextResponse.json({
+    revalidate: true,
+    timestamp: Date.now(),
+  });
 }
